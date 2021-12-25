da131fe11e84fd13cdf685b3f16b628e596f2177
Bryan Elliott <bryan_elliott@comcast.com>
2021-12-25 12:49:25 -0500

    big and centered

--- docs/App.js
+++ docs/App.js
@@ -34,42 +34,48 @@ export default () => {
   });
   const [textId, passwordId] = useUids(2);
   return html`
-    <${Title}>Meater Monitor</>
-    ${loggedIn && (
-      devices.length
-      ? html`
-        ${devices.map(device => html`
-          <${MeaterDevice} ...${device} />
+    <div className=${styles.main} style=${{
+      zoom: document.documentElement.clientHeight / 230 * 0.96,
+      width: 200,
+      margin: '0 auto',
+    }}>
+      <${Title}>Meater Monitor</>
+      ${loggedIn && (
+        devices.length
+        ? html`
+          ${devices.map(device => html`
+            <${MeaterDevice} ...${device} />
+            <button onClick=${logout}>Log out</button>
+          `)}
+        `
+        : html`
+          <div>No active Meater devices.</div>
           <button onClick=${logout}>Log out</button>
-        `)}
-      `
-      : html`
-        <div>No active Meater devices.</div>
-        <button onClick=${logout}>Log out</button>
-      `
-    )}
-    
-    ${!loggedIn && html`
-      <form className=${styles.loginForm} onSubmit=${signIn}>
-        <h2>Sign into Meater Cloud</h2>
-        <p>(your credentials are not stored, but sent directly to Meater)</p>
-        ${error && html`
-          <div className=${styles.error}>${error}</div>
-        `}
-        <div>
-          <label htmlFor=${textId}>
-            Email: 
-            <input type="text" name="email" id=${textId} />
-          </label>
-        </div>
-        <div>
-          <label htmlFor=${passwordId}>
-            Password:
-            <input type="password" name="password" id=${passwordId} />
-          </label>
-        </div>
-        <button type="submit">Log in</button>
-      </form>
-    `}
+        `
+      )}
+      
+      ${!loggedIn && html`
+        <form className=${styles.loginForm} onSubmit=${signIn}>
+          <h2>Sign into Meater Cloud</h2>
+          <p>(your credentials are not stored, but sent directly to Meater)</p>
+          ${error && html`
+            <div className=${styles.error}>${error}</div>
+          `}
+          <div>
+            <label htmlFor=${textId}>
+              Email: 
+              <input type="text" name="email" id=${textId} />
+            </label>
+          </div>
+          <div>
+            <label htmlFor=${passwordId}>
+              Password:
+              <input type="password" name="password" id=${passwordId} />
+            </label>
+          </div>
+          <button type="submit">Log in</button>
+        </form>
+      `}
+    </div>
   `;
 };
