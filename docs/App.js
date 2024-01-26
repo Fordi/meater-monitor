cb70157c26810767a08d7524d9ba69e92d86cd2a
Bryan Elliott <bryan.elliott@cesium.com>
2024-01-26 09:39:58 -0500

    Fixed for multiple meaters

--- docs/App.js
+++ docs/App.js
@@ -1,14 +1,14 @@
-import html from 'html';
-import css from 'css';
-import useUids from 'useUids';
-import collectForm from 'collectForm';
+import html from "html";
+import css from "css";
+import useUids from "useUids";
+import collectForm from "collectForm";
 
-import { useContext, useState, useCallback } from 'preact/hooks';
-import useEventListener from 'useEventListener';
+import { useContext, useState, useCallback, useEffect } from "preact/hooks";
+import useEventListener from "useEventListener";
 
-import Title from './Title.js';
-import MeaterCloud from './MeaterCloud.js';
-import MeaterDevice from './MeaterDevice.js';
+import Title from "./Title.js";
+import MeaterCloud from "./MeaterCloud.js";
+import MeaterDevice from "./MeaterDevice.js";
 
 const styles = css`
   .loginForm {
@@ -19,12 +19,11 @@ const styles = css`
   }
   .main {
     margin: 0 auto;
-    width: 200px;
   }
   button {
     background-color: #222;
-    border: 2px solid #EEE;
-    color: #EEE;
+    border: 2px solid #eee;
+    color: #eee;
     border-radius: 2px;
     padding: 0.5em 1em;
   }
@@ -33,71 +32,108 @@ const styles = css`
     bottom: 0;
     right: 0;
   }
+  .meaterGrid {
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
 `;
 
-const calculateZoom = () => Math.min(
-  document.documentElement.clientHeight / 230 * 0.96,
-  document.documentElement.clientWidth / 200 * 0.96,
-);
+const GRID = [200, 230];
 
 export default () => {
-  const [zoom, setZoom] = useState(calculateZoom);
   const { devices, loggedIn, login, logout } = useContext(MeaterCloud);
-  const [error, setError] = useState(null);
-  const signIn = useCallback(async (event) => {
-    event.preventDefault();
-    const { email, password } = collectForm(event.target);
-    try {
-      await login(email, password);
-    } catch (error) {
-      setError(error.message);
+  const calculateZoom = useCallback(() => {
+    const screenAspect =
+      document.documentElement.clientWidth /
+      document.documentElement.clientHeight;
+    const gridAspect = GRID[0] / GRID[1];
+    const squareAspect = screenAspect / gridAspect;
+    let repeatHeight, repeatWidth;
+    if (squareAspect < 1) {
+      repeatHeight = Math.ceil(Math.sqrt(devices.length / squareAspect));
+      repeatWidth = Math.floor(devices.length / repeatHeight);
+      repeatHeight = Math.ceil(devices.length / repeatWidth);
+    } else {
+      repeatWidth = Math.ceil(Math.sqrt(devices.length / squareAspect));
+      repeatHeight = Math.floor(devices.length / repeatWidth);
+      repeatWidth = Math.ceil(devices.length / repeatHeight);
     }
-  });
-  useEventListener(window, 'resize', () => setZoom(calculateZoom));
+
+    return Math.min(
+      (document.documentElement.clientWidth / (repeatWidth * GRID[0])) * 0.9,
+      (document.documentElement.clientHeight / (repeatHeight * GRID[1])) * 0.9
+    );
+  }, [devices.length]);
+  const [zoom, setZoom] = useState(calculateZoom);
+
+  const [error, setError] = useState(null);
+  const signIn = useCallback(
+    async (event) => {
+      event.preventDefault();
+      const { email, password } = collectForm(event.target);
+      try {
+        await login(email, password);
+      } catch (error) {
+        setError(error.message);
+      }
+    },
+    [login]
+  );
+  useEventListener(window, "resize", () => setZoom(calculateZoom));
+  useEffect(() => {
+    setZoom(calculateZoom);
+  }, [calculateZoom]);
   const [textId, passwordId] = useUids(2);
   return html`
-    ${loggedIn && html`
-      <button onClick=${logout} className=${styles.logout}>Log out</button>
-    `}
+    ${
+      loggedIn &&
+      html`
+        <button onClick=${logout} className=${styles.logout}>Log out</button>
+      `
+    }
     <div className=${styles.main} style=${{ zoom }}>
-      <${Title}>Meater Monitor</>
-      ${loggedIn && (
-        devices.length
-        ? html`
-          ${devices.map(device => {
-            console.log(device);
-            return html`
-              <${MeaterDevice} ...${device} />
-            `;
-          })}
-        `
-        : html`
-          <div>No active Meater devices.</div>
-        `
-      )}
-      
-      ${!loggedIn && html`
-        <form className=${styles.loginForm} onSubmit=${signIn}>
-          <h2>Sign into Meater Cloud</h2>
-          <p>(your credentials are not stored, but sent directly to Meater)</p>
-          ${error && html`
-            <div className=${styles.error}>${error}</div>
-          `}
-          <div>
-            <label htmlFor=${textId}>
-              Email: 
-              <input type="text" name="email" id=${textId} />
-            </label>
+      <${Title}>Meater Monitor</${Title}>
+      ${
+        loggedIn &&
+        html`
+          <div className=${styles.meaterGrid}>
+            ${devices.length
+              ? html`
+                  ${devices.map(
+                    (device) => html`<${MeaterDevice} ...${device} /> `
+                  )}
+                `
+              : html` <div>No active Meater devices.</div> `}
           </div>
-          <div>
-            <label htmlFor=${passwordId}>
-              Password:
-              <input type="password" name="password" id=${passwordId} />
-            </label>
-          </div>
-          <button type="submit">Log in</button>
-        </form>
-      `}
+        `
+      }
+      ${
+        !loggedIn &&
+        html`
+          <form className=${styles.loginForm} onSubmit=${signIn}>
+            <h2>Sign into Meater Cloud</h2>
+            <p>
+              (your credentials are not stored, but sent directly to Meater)
+            </p>
+            ${error && html` <div className=${styles.error}>${error}</div> `}
+            <div>
+              <label htmlFor=${textId}>
+                Email:
+                <input type="text" name="email" id=${textId} />
+              </label>
+            </div>
+            <div>
+              <label htmlFor=${passwordId}>
+                Password:
+                <input type="password" name="password" id=${passwordId} />
+              </label>
+            </div>
+            <button type="submit">Log in</button>
+          </form>
+        `
+      }
     </div>
   `;
 };
